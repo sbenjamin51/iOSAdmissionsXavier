6becd1e5022099bf840638a8fc94ab3438ff89b7
sbenjamin51 <benjamins@xavier.edu>
2024-09-30 12:02:30 -0400

    Initial Commit

---
+++ iOSAdmissionsXavier/iOSAdmissionsXavierApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSAdmissionsXavierApp.swift
+//  iOSAdmissionsXavier
+//
+//  Created by Simon Benjamin on 9/30/24.
+//
+
+import SwiftUI
+
+@main
+struct iOSAdmissionsXavierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iOSAdmissionsXavierTests/iOSAdmissionsXavierTests.swift
@@ -0,0 +1,17 @@
+//
+//  iOSAdmissionsXavierTests.swift
+//  iOSAdmissionsXavierTests
+//
+//  Created by Simon Benjamin on 9/30/24.
+//
+
+import Testing
+@testable import iOSAdmissionsXavier
+
+struct iOSAdmissionsXavierTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
